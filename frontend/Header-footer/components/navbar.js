49530869ab466120bff89b967d8d5d221bf932b8
Raoji Patil <raojipatil22@gmail.com>
2022-01-23 15:39:39 +0530

    link part completed

--- frontend/Header-footer/components/navbar.js
+++ frontend/Header-footer/components/navbar.js
@@ -16,15 +16,15 @@ function navbar() {
        <div id="nabvar_right">
            <div id="cart_logo2">
                <img id="cart_logo" src="https://cdn-icons.flaticon.com/png/512/2618/premium/2618267.png?token=exp=1642564688~hmac=2b895175e79162a317f950498831af13" alt="">
-               <p id="order">My orders</p>
+               <p id="order"><a href="/frontend/html/cart.html">My orders</a></p>
            </div>
            <div id="cart_logo2">
                <img id="cart_logo1" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSwdVLvMRdRaZxmJQi9OdckXZiuQlxogw3CA&usqp=CAU" alt="">
-               <p id="cart_logo3">Cart</p>
+               <p id="cart_logo3"><a href="/frontend/html/cart.html">Cart</a></p>
            </div>
            <div id="cart_logo2">
            <img id="cart_logo4" src="http://cdn.onlinewebfonts.com/svg/img_337531.png">
-               <p id="cart_logo5">Log In/Sign Up</p>
+               <p id="cart_logo5" ><a href="/frontend/html/signup.html">Log In/Sign Up</a></p>
            </div>
        </div>
 
